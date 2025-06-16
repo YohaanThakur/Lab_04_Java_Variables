d7c3773e38d19c19ce13131d5f3a2a69eda3e8ca
YohaanThakur <thakuryn@mail.uc.edu>
2025-06-15 20:45:45 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+void main() {
+    int intOperandA = 1;
+    int intOperandB = 2;
+    int intsum = 3;
+    int intSum = intOperandA + intOperandB;
+    System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+}
