3358035478b99abef1619a2b116ef7a1d8da419a
YohaanThakur <thakuryn@mail.uc.edu>
2025-06-16 12:51:32 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 void main() {
-    int intOperandA = 1;
-    int intOperandB = 2;
-    int intsum = 3;
-    int intSum = intOperandA + intOperandB;
-    System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+    int kidsFamily = 1;
+    boolean isRaining = true;
+    int gallonGas = 4;
+    int favNumber = 83;
+    int shoeSize = 12;
+    String birthMonth = "June";
+    String fullName = "Yohaan Thakur";
+
 }
